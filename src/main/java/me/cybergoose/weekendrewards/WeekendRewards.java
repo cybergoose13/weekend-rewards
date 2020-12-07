6c2cce4619339be626241b9185ee46f566b3226a
BuildTools <cybergoose13@gmail.com>
2020-12-07 13:27:35 -0800

    added config.yml file

--- src/main/java/me/cybergoose/weekendrewards/WeekendRewards.java
+++ src/main/java/me/cybergoose/weekendrewards/WeekendRewards.java
@@ -1,18 +1,33 @@
+/*  Title:      Weekend Rewards
+*   Author:     CyberGoose
+*   Start:      7-12-20
+*   Update:     7-12-20
+*   Version:    Snap-1.0
+* */
+
 package me.cybergoose.weekendrewards;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WeekendRewards extends JavaPlugin {
 
+    private final ConsoleCommandSender CONSOLESENDER= this.getServer().getConsoleSender();
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + this.getName() + " is enabled.");
+        loadConfig();
+        CONSOLESENDER.sendMessage(ChatColor.GREEN + this.getName() + " is enabled.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        CONSOLESENDER.sendMessage(ChatColor.RED + this.getName() + " is disabled.");
+    }
+
+    private void loadConfig(){
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
 }
